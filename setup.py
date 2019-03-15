45aa85fb75db1f1347b323ef5f65c81f23020841
Mário Hunka <hunka.mario@gmail.com>
2019-03-15 22:56:21 +0100

    Add travis.

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Ultimite guide to life, python and all.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/maroshmka/zen-of-koli',
     author='Mario Hunka',
     author_email='hunka.mario@gmail.com'
