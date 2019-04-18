bb069d4c41f79384b3f705ddf5ae5cdf8305cb04
Mário Hunka <hunka.mario@gmail.com>
2019-04-18 16:23:10 +0200

    Change package names. Add new wisdom.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zen-of-koli',
-    version='0.1',
+    version='0.0.7',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Ultimite guide to life, python and all.',
